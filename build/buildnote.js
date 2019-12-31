9f428daaba3cee41975566fba5131644caabf8b4
liuhao <czliuhao@qq.com>
2019-12-31 23:29:27 +0800

    relative path

--- build/buildnote.js
+++ build/buildnote.js
@@ -56,7 +56,7 @@ function buildFile(filePath, noteList) {
     let fileName = fileInfo && fileInfo.fileName || 'note';
     let fileTitle = getMarkDownTitle(filePath);
     noteList.push({
-        sourceFilePath: path.resolve(path.resolve(__dirname, '..'), filePath),
+        sourceFilePath: path.relative(path.resolve(__dirname, '..'), filePath),
         link: `/note/dist/${fileName}.html`,
         name: fileTitle
     });
